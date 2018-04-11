e5c0c9bd3dd2fd3f72d0f839f3565dce36b3ca94
l-ChengYou-l <l-chenchen-l@hotmail.com>
2018-04-11 20:55:38 +0800

    taxi计价

--- main/main.js
+++ main/main.js
@@ -1,4 +1,12 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function taxiFee(inpuuts) {
+    let price = 0
+    if(inpuuts.distance <= 2){
+        price = 6+inpuuts.parkTime*0.25
+    }else if(inpuuts.distance>2&&inpuuts.distance<=8){
+        price = 6+(inpuuts.distance-2)*0.8+inpuuts.parkTime*0.25
+    }else{
+        price = Number((6+6*0.8+(inpuuts.distance-8)*0.8*1.5+inpuuts.parkTime*0.25).toFixed(1))
+    }
+    console.log(price);
+    return price;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,23 @@
-const main = require('../main/main');
+const taxiFee = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    it('return price within 2km',function(){
+        let inputs = {distance:1.5,parkTime:4}
+        let summary = taxiFee(inputs)
+        let expected = 7
+        expect(summary).toEqual(expected)
+    });
+    it('return price between 2~8km',function(){
+        let inputs = {distance:6,parkTime:4}
+        let summary = taxiFee(inputs)
+        let expected = 10.2
+        expect(summary).toEqual(expected)
+    });
+    it('return price over 8km',function(){
+        let inputs = {distance:20,parkTime:8}
+        let summary = taxiFee(inputs)
+        let expected = 27.2
+        expect(summary).toEqual(expected)
+    });
 });
